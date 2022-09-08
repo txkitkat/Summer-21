00be2bd48ce4bb8b1af3c7f6abff8af6e7948d92
TiffanyM <mejia9tiffany@gmail.com>
2022-09-07 21:00:09 -0700

    test

--- Python/pets.py
+++
@@ -1,4 +0,0 @@
-# Initial Python project
-# started 6-18-21
-
-print ("Hello, World!")
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/opencv.hpp"
+#include <iostream>
+using namespace cv;
+
+int main(int argc, char** argv) 
+{ 
+ // Read the image file 
+ Mat image = imread("C:/Users/lilf/Pictures/cat.jpg"); 
+//  namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
+//  imshow("Display Image", image);
+//  waitKey(0);
+ return 0; 
+}
